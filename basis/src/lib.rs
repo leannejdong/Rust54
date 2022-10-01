3a119304b20a40aa8d49a10cf7a71ba55158b2ea
Leanne J Dong <jdleanne@gmail.com>
2022-10-02 03:21:45 +1100

    update

--- basis/src/lib.rs
+++ basis/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod borrowing;
+pub mod struc;
--- basis/src/main.rs
+++ basis/src/main.rs
@@ -5,6 +5,8 @@ use basis::borrowing::statics;
 use basis::borrowing::usize_example;
 use basis::borrowing::get_first_element;
 use basis::borrowing::fnStr;
+use basis::struc::User;
+use basis::struc::build_user;
 
 
 fn main(){
@@ -36,6 +38,39 @@ fn main(){
     let s22 = &s11;
     println!("{:?}", s22);
 
+    let mut user1 = User {
+      email: String::from("1@example.com"),
+      username: String::from("1username123"),
+      active: true,
+      sign_in_count: 1,
+  };
+  
+  user1.email = String::from("anotheremail@example.com");
 
+  println!("{:?}", user1);
+  
+  let user2 = User {
+    email: String::from("2@example.com"),
+    username: String::from("2username123"),
+    active: true,
+    sign_in_count: 1,
+  };
+  
+  println!("{:?}", user2);
+
+  
+  let user3 = User{
+    email: String::from("3@example.com"),
+    ..user2
+  };
+
+  println!("{:?}", user3);
+
+    /*It's more idiomatic to use to_owned() or into() (in case the target type is known) rather than to_string(). The latter works through Display trait which invokes 
+    formatting code which introduces some overhead */
+  let user4 = build_user("leanne@54.com".to_string(), "leanne".to_owned());
+
+
+  println!("{:?}", user4);
 
 }
